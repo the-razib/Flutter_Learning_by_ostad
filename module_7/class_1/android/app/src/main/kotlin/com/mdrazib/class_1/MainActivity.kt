38a313b3d014d1685ebbc6500f8fbbbfcf2b5c0f
Coder-boss-debug <mrajibulislam534@gmail.com>
2024-07-04 20:01:04 +0600

    first commit

---
+++ module_7/class_1/android/app/src/main/kotlin/com/mdrazib/class_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mdrazib.class_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ module_7/class_1/lib/main.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(IntroApp());
+}
+
+// here we create a widget name IntroApp
+class IntroApp extends StatelessWidget {  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          title: Text('Home'),
+        ),
+        body:  Center(
+          child: Text('My name is md razib . I study in daffodil international univeristy',
+          textAlign: TextAlign.center,
+          maxLines: 1,
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.blueAccent,
+            fontWeight: FontWeight.w400,
+            decoration: TextDecoration.underline,
+            letterSpacing: 2,
+            overflow: TextOverflow.ellipsis,
+
+          ),
+          ),
+        ),
+      ),
+    );
+  }
+}
